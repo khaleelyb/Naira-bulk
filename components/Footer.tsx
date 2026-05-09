5c4c9878c16ea4d8cd285558f1af135b0d0d6e58
khaleelyb <ikhalil520@gmail.com>
2026-05-09 07:08:30 +0100

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,10 @@
-
 import React from 'react';
 
 export const Footer: React.FC = () => {
   return (
     <footer className="bg-gray-800 dark:bg-gray-900 text-white">
       <div className="container mx-auto px-4 py-6 text-center">
-        <p>&copy; 2026 Kano Marketplace. All rights reserved.</p>
+        <p>&copy; 2026 NairaBulk Marketplace. All rights reserved.</p>
       </div>
     </footer>
   );
